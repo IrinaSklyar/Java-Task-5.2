341c586a4dbeaadcf1506bc8189e95345aaf5363
Irina Skliarova <i.skliarova2015@yandex.ru>
2020-04-18 20:24:30 +0300

    Fixed #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,12 +1,11 @@
 public class Main {
-  public static void main(String[] args) {
-    BonusService service = new BonusService();
+    public static void main(String[] args) {
+        BonusService service = new BonusService();
+        long amount = 1000_60;
+        boolean registered = true;
+        long bonus = service.calculate(1000_60, true);
+        System.out.println(bonus);
 
-    long amount = 1000_60;
-    boolean registered = true;
-
-    service.calculate(amount, registered);
-
-  }
+    }
 }
 
